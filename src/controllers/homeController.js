88629272bac3c5998669a2b7ad12112927a2a377
Nguyễn Ngọc Quân <qkt1122002@gmail.com>
2024-12-03 16:18:59 +0700

    #38

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -22,8 +22,13 @@ const getQuanBeoIoT = (req, res) => {
 }
 
 const postAddUser = (req, res) => {
-    console.log(req.body);
-    return res.send('Create User');
+    let{email, name, city} = req.body;
+    connection.query(`INSERT INTO Users (email, name, city) VALUES (?, ?, ?);`, [email, name, city],
+        function(err, results){
+            console.log(req.body);
+            res.render('index');
+        }
+    )
 }
 module.exports = {
     getHomepage, getABC, getQuanBeoIoT, getAPI,
