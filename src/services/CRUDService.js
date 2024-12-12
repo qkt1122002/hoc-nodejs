2a4946028cf418ec0f27859681811705e19901fd
Nguyễn Ngọc Quân <qkt1122002@gmail.com>
2024-12-12 19:25:09 +0700

    test nhanh moi

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -17,6 +17,7 @@ const getUserByID = async (id) => {
 
 const deleteUserById = async (id) => {
     let [results, fields] = await connection.query('DELETE FROM Users WHERE id = ?', [id]);
+    let a = 2;
 }
 module.exports = { getAllUsers, updateUser, getUserByID, deleteUserById };
 
