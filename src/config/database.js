7cdf4d6a3e08c120bcb18ed8d60bdfc81a4ce00a
Nguyễn Ngọc Quân <qkt1122002@gmail.com>
2024-12-13 18:06:37 +0700

    #59

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,16 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 const mongoose = require('mongoose');
 
 const connection = async () => {
-    try {
-        await mongoose.connect('mongodb://root:123456@localhost:27017/');
-        const state = mongoose.connection.readyState;
-        console.log("Trang thai: ", state);
+    const options = {
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASSWORD,
         
-    }catch (error){
-        console.log('Error connection DB: ', error);
     }
+    await mongoose.connect(process.env.DB_HOST, options);
+    const state = mongoose.connection.readyState;
+    console.log("Trang thai: ", state);
 }
 
 module.exports = connection
--- src/index.js
+++ src/index.js
@@ -6,11 +6,26 @@ const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 const connection = require('./config/database');
+const { default: mongoose } = require('mongoose');
 configViewEngine(app);
 
 app.use('/', webRoutes);
 
-connection()
-app.listen(port, hostname, () => {
-    console.log(`Listen on port ${port}`);
+const kitty = new mongoose.Schema({
+    name: String
 });
+const Kitten = mongoose.model('Kitten', kitty);
+const cat = new Kitten({name: 'Quan Beo IoT Cat'});
+cat.save();
+
+(async () => {
+    try {
+        await connection();
+        app.listen(port, hostname, () => {
+            console.log(`Listen on port ${port}`);
+        });
+    } catch (error) {
+        console.log("Error: ", error);
+        
+    }
+})();
